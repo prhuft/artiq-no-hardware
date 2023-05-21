71ac5a69916753e219871dd15fc7380129e6b682
Preston Huft <gothrowayo@gmail.com>
2023-05-21 16:55:20 -0500

    map hardware channel names to aliases in an experiment

---
+++ device_aliases.py
@@ -0,0 +1,51 @@
+"""
+Map devices defined in a parent experiment to aliases
+"""
+
+from artiq.experiment import *
+
+
+class DeviceAliases:
+
+    alias_map = {
+        "urukul0_ch0": "dds_FORT",
+        "urukul0_ch1": "dds_cooling_SP"
+    }
+
+    def __init__(self, experiment):
+        for key,val in self.alias_map.items():
+            if hasattr(experiment, key):
+                print(f"setting attr {key} to {val}")
+                setattr(experiment, val, getattr(experiment,key))
+                
+class FakeSwitch:
+
+    def on(self):
+        print("on")
+        
+    def off(self):
+        print("off")
+            
+class FakeUrukul:
+
+    def __init__(self):
+        self.sw = FakeSwitch()
+              
+# test class              
+class FakeExperiment(EnvExperiment):
+
+    def build(self):
+        # with actual hardware we would use self.setattr_device("device")
+        self.urukul0_ch0 = FakeUrukul()
+        
+        # map the hardware channels
+        DeviceAliases(experiment=self)
+        assert hasattr(self, DeviceAliases.alias_map['urukul0_ch0']), f"whoops, still missing attr {DeviceAliases.alias_map['urukul0_ch0']}"
+    
+    def run(self):
+        self.dds_FORT.sw.on()
+        print("FORT dds on")
+
+    
+    
+    
